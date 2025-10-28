643036e63882f33c4041f70b0fcd7b480eae170c
Hugo Casanova <hola@hugotech.pe>
2025-10-28 00:42:26 -0500

    fix: cambiar itemReviewed de Service a Organization en Review schema
    - Service no admite reseñas según Schema.org
    - Cambiar a Organization que SÍ acepta Review
    - Resolver error "El elemento no admite reseñas" de Google Search Console
    - author: Person ✓, itemReviewed: Organization ✓, reviewBody ✓

--- components/cards/TestimonioCard.tsx
+++ components/cards/TestimonioCard.tsx
@@ -114,8 +114,8 @@ export const TestimonioCard: React.FC<TestimonioCardProps> = ({
             <div itemProp="author" itemScope itemType="https://schema.org/Person">
               <meta itemProp="name" content={authorName} />
             </div>
-            <div itemProp="itemReviewed" itemScope itemType="https://schema.org/Service">
-              <meta itemProp="name" content="HugoTech - Mentoring y Coaching Tech" />
+            <div itemProp="itemReviewed" itemScope itemType="https://schema.org/Organization">
+              <meta itemProp="name" content="HugoTech" />
             </div>
             
             <blockquote
